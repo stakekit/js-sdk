299a450a695df5b4b46190e928342ac80c478ce7
petar-omni <117348982+petar-omni@users.noreply.github.com>
2023-12-20 12:04:28 +0100

    feat(): schema changes (#62)

--- packages/api-hooks/src/api/schemas/yieldBalanceDto.ts
+++ packages/api-hooks/src/api/schemas/yieldBalanceDto.ts
@@ -5,6 +5,7 @@ import type { BalanceTypes } from './balanceTypes';
 export interface YieldBalanceDto {
   amount: string;
   date?: string;
+  groupId: string;
   pendingActions: PendingActionDto[];
   /** The price of the reward token as a multiple of the underlying token. Used when dealing with index based yields where the reward asset appreciates in value over time. */
   pricePerShare: string;
