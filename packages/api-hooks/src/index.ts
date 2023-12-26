1281717ba5ffb8116a9ed80e1a223e1e3f0d07f1
Petar Todorovic <petar@omni.app>
2023-12-26 13:13:39 +0100

    feat(): add mocks

--- packages/api-hooks/src/index.ts
+++ packages/api-hooks/src/index.ts
@@ -3,3 +3,4 @@ export * from './api/schemas';
 export * from './query-provider';
 export { APIManager } from './api-client';
 export * from '@tanstack/react-query';
+export * from './api/index.msw';
