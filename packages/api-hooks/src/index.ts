c04d62c4648ec308081af8e73f1ec2cc31c8f21c
petar-omni <117348982+petar-omni@users.noreply.github.com>
2024-01-04 10:42:54 +0100

    feat(): schema changes (#67)

--- packages/api-hooks/src/index.ts
+++ packages/api-hooks/src/index.ts
@@ -1,6 +1,7 @@
+export * from '@tanstack/react-query';
+export * from 'axios';
 export * from './api';
 export * from './api/schemas';
 export * from './query-provider';
 export { APIManager } from './api-client';
-export * from '@tanstack/react-query';
 export * from './api/index.msw';
