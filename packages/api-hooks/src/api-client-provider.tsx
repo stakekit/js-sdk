7d17764f82986045cb93866051256900f760bccd
Petar Todorovic <petar@omni.app>
2024-03-21 13:05:44 +0100

    chore(): revamp

---
+++ packages/api-hooks/src/api-client-provider.tsx
@@ -0,0 +1,21 @@
+import { AxiosInstance } from 'axios';
+import React, { PropsWithChildren, createContext, useContext } from 'react';
+
+const Context = createContext<AxiosInstance | undefined>(undefined);
+
+export const ApiClientProvider = ({
+  apiClient,
+  children,
+}: PropsWithChildren<{ apiClient: AxiosInstance }>) => {
+  return <Context.Provider value={apiClient}>{children}</Context.Provider>;
+};
+
+export const useApiClient = () => {
+  const value = useContext(Context);
+
+  if (!value) {
+    throw new Error('ApiClient must be used within a ApiHooksProvider');
+  }
+
+  return value;
+};
