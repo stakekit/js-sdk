efb3ecbf4a702accf522422f23805ac78f3aede3
Petar Todorovic <petar@omni.app>
2024-05-17 12:35:03 +0200

    feat(): schema changes

--- packages/api-hooks/src/api/schemas/yieldBalanceDto.ts
+++ packages/api-hooks/src/api/schemas/yieldBalanceDto.ts
@@ -9,6 +9,7 @@ export interface YieldBalanceDto {
   pendingActions: PendingActionDto[];
   /** The price of the reward token as a multiple of the underlying token. Used when dealing with index based yields where the reward asset appreciates in value over time. */
   pricePerShare: string;
+  providerId?: string;
   token: TokenDto;
   type: BalanceTypes;
   validatorAddress?: string;
