534671a2d372c35dea13cb32c751ca6d1e466091
Philip Zudemberg <philip.zdb@gmail.com>
2024-07-02 14:27:24 +0200

    feat: regenerate api hooks (#102)
    * feat: regenerate api hooks
    
    * chore: publish

--- packages/api-hooks/src/api/schemas/yieldBalanceDto.ts
+++ packages/api-hooks/src/api/schemas/yieldBalanceDto.ts
@@ -1,3 +1,4 @@
+import type { YieldBalanceLabelDto } from './yieldBalanceLabelDto';
 import type { PendingActionDto } from './pendingActionDto';
 import type { TokenDto } from './tokenDto';
 import type { BalanceTypes } from './balanceTypes';
@@ -6,6 +7,7 @@ export interface YieldBalanceDto {
   amount: string;
   date?: string;
   groupId: string;
+  label?: YieldBalanceLabelDto;
   pendingActions: PendingActionDto[];
   /** The price of the reward token as a multiple of the underlying token. Used when dealing with index based yields where the reward asset appreciates in value over time. */
   pricePerShare: string;
