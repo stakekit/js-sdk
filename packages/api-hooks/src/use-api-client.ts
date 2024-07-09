2ff2b7f47a00a5fdd883c1984c634a1530dc563b
Petar Todorovic <petar@omni.app>
2024-07-09 14:50:56 +0200

    feat(): remove axios

--- packages/api-hooks/src/use-api-client.ts
+++ packages/api-hooks/src/use-api-client.ts
@@ -1,9 +1,49 @@
-import { AxiosRequestConfig } from 'axios';
-import { useApiClient } from './api-client-provider';
+import { useApiClientProps } from './api-client-provider';
+
+const defaultBaseURL = 'https://api.stakek.it/';
+
+const getUrl = ({
+  baseURL,
+  path,
+  params,
+}: {
+  baseURL: string;
+  path: string;
+  params?: any;
+}): string => {
+  const url = new URL(baseURL);
+  url.pathname = path;
+  url.search = new URLSearchParams(params).toString();
+
+  return url.toString();
+};
 
 export const useApi = <T>() => {
-  const apiClient = useApiClient();
+  const {
+    fetchInstance,
+    apiKey,
+    baseURL = defaultBaseURL,
+  } = useApiClientProps();
 
-  return (config: AxiosRequestConfig): Promise<T> =>
-    apiClient(config).then((response) => response.data);
+  return ({
+    url,
+    data,
+    method,
+    params,
+    headers,
+    signal,
+  }: {
+    url: string;
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+    params?: any;
+    data?: Record<string, any>;
+    headers?: Record<string, any>;
+    signal?: AbortSignal;
+  }): Promise<T> =>
+    fetchInstance<T>(getUrl({ baseURL, path: url, params }), {
+      method,
+      data,
+      headers: { ...headers, 'X-API-KEY': apiKey },
+      signal,
+    });
 };
