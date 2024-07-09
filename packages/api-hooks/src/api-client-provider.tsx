675f32d70ff83c3302fada3f0466674fbf370358
petar-omni <117348982+petar-omni@users.noreply.github.com>
2024-07-09 14:51:25 +0200

    feat(): remove axios (#104)

--- packages/api-hooks/src/api-client-provider.tsx
+++ packages/api-hooks/src/api-client-provider.tsx
@@ -1,16 +1,29 @@
-import { AxiosInstance } from 'axios';
 import { PropsWithChildren, createContext, useContext } from 'react';
 
-const Context = createContext<AxiosInstance | undefined>(undefined);
-
-export const ApiClientProvider = ({
-  apiClient,
-  children,
-}: PropsWithChildren<{ apiClient: AxiosInstance }>) => {
-  return <Context.Provider value={apiClient}>{children}</Context.Provider>;
+type ApiClientProps = {
+  fetchInstance: <T>(
+    url: string,
+    requestInit: {
+      method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+      data?: Record<string, any>;
+      headers?: Record<string, any>;
+      signal?: AbortSignal;
+    },
+  ) => Promise<T>;
+  baseURL?: string;
+  apiKey: string;
 };
 
-export const useApiClient = () => {
+const Context = createContext<ApiClientProps | undefined>(undefined);
+
+export const ApiClientProvider = ({
+  children,
+  ...apiClientProps
+}: PropsWithChildren<ApiClientProps>) => (
+  <Context.Provider value={apiClientProps}>{children}</Context.Provider>
+);
+
+export const useApiClientProps = () => {
   const value = useContext(Context);
 
   if (!value) {
