d47ad16d557786830c301087e103b00034c7cc7d
Petar Todorovic <petar@stakek.it>
2025-04-30 10:57:18 +0200

    feat: schema changes

--- packages/api-hooks/src/api/schemas/tokenGetTokensParams.ts
+++ packages/api-hooks/src/api/schemas/tokenGetTokensParams.ts
@@ -1,5 +1,9 @@
 import type { Networks } from './networks';
 
 export type TokenGetTokensParams = {
+  /**
+   * When true, returns only tokens that are related to enabled yields
+   */
+  enabledYieldsOnly?: boolean;
   network?: Networks;
 };
