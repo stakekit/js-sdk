0ffa5d009c9a7fff171d00fea5ce5ec4d611a6d6
petar-omni <117348982+petar-omni@users.noreply.github.com>
2025-06-06 15:39:07 +0200

    feat: schema changes (#149)

--- packages/api-hooks/src/api/schemas/yieldV2YieldsParams.ts
+++ packages/api-hooks/src/api/schemas/yieldV2YieldsParams.ts
@@ -1,9 +1,14 @@
+import type { YieldV2YieldsProviderId } from './yieldV2YieldsProviderId';
 import type { YieldV2YieldsType } from './yieldV2YieldsType';
 import type { YieldV2YieldsSortBy } from './yieldV2YieldsSortBy';
 import type { YieldV2YieldsRevenueOption } from './yieldV2YieldsRevenueOption';
 import type { YieldV2YieldsNetwork } from './yieldV2YieldsNetwork';
 
 export type YieldV2YieldsParams = {
+  /**
+   * Filter yields by provider ID
+   */
+  providerId?: YieldV2YieldsProviderId;
   /**
    * Filter yields by input token. Use "native" for native tokens (ETH, AVAX, BNB, etc.) or provide a contract address
    */
